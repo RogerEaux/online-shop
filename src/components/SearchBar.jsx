e5a2ce58ae47b298ac6c7d47e55b8b1344c75cc2
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-09 23:31:08 -0600

    Add styles to SearchBarComponent

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,17 @@
 import searchIcon from '../assets/search.svg';
+import styles from '../styles/SearchBar.module.css';
 
 function SearchBar() {
   return (
     <>
-      <label>
-        <input type="text" id="searchBox" placeholder="Search for item..." />
-        <button>
+      <label className={styles.search}>
+        <input
+          className={styles.searchBox}
+          type="text"
+          id="searchBox"
+          placeholder="Search for item..."
+        />
+        <button className={styles.searchButton}>
           <img src={searchIcon} alt="Search" />
         </button>
       </label>
