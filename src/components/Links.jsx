b07f6bbe2ed0f3118da49cff5279bf0dd180fd3e
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-10 21:25:56 -0600

    Add Links component

---
+++ src/components/Links.jsx
@@ -0,0 +1,16 @@
+function Links() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Shop</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Links;
