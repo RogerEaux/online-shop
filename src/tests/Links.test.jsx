09aa1ea7927bd4e57a6d29c8721c28728debda56
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-10 22:53:07 -0600

    Wrap render in Router to allow router components

--- src/tests/Links.test.jsx
+++ src/tests/Links.test.jsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Links from '../components/Links';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Links component', () => {
   it('renders a list of links', () => {
-    render(<Links />);
+    render(
+      <BrowserRouter>
+        <Links />
+      </BrowserRouter>,
+    );
     expect(screen.getByRole('list')).toBeInTheDocument();
     expect(screen.getByRole('list')).toContainElement(
       screen.getByRole('link', { name: /home/i }),
