491a1b750f253c4260c01205c443a4e1f1dc7423
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-10 21:56:02 -0600

    Add styles to Links component

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,11 +1,13 @@
-function Links() {
+import styles from '../styles/Links.module.css';
+
+function Links({ direction }) {
   return (
     <>
-      <ul>
-        <li>
+      <ul className={`${styles[direction]}`}>
+        <li className={styles.linksItem}>
           <a href="">Home</a>
         </li>
-        <li>
+        <li className={styles.linksItem}>
           <a href="">Shop</a>
         </li>
       </ul>
