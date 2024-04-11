c936f3dd89604e00b74e09a35808e55ab9e3ebe4
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-10 23:10:20 -0600

    Add shopping cart button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Links from './Links';
 import SearchBar from './SearchBar';
 import logo from '../assets/logo-placeholder.png';
+import cart from '../assets/cart.svg';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
@@ -11,6 +12,9 @@ function NavBar() {
       </Link>
       <SearchBar />
       <Links direction="row" />
+      <button>
+        <img src={cart} alt="Shopping Cart" />
+      </button>
     </>
   );
 }
