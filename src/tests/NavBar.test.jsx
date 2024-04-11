745f6a8a6c41fe87cd855041bbd8768183771bce
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-10 23:10:01 -0600

    Test NavBar renders shopping cart button

--- src/tests/NavBar.test.jsx
+++ src/tests/NavBar.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import NavBar from '../components/NavBar';
 import { BrowserRouter } from 'react-router-dom';
+import { expect } from 'vitest';
 
 describe('NavBar component', () => {
   it('renders logo of fake company', () => {
@@ -12,5 +13,16 @@ describe('NavBar component', () => {
     expect(
       screen.getByRole('link', { name: /fake company/i }),
     ).toContainElement(screen.getByRole('img', { name: /fake company/i }));
+  });
+
+  it('renders shopping cart button', () => {
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>,
+    );
+    expect(
+      screen.getByRole('button', { name: /shopping cart/i }),
+    ).toBeInTheDocument();
   });
 });
