f6922aa7ff3403cb9363cdf801ae9ae84322e8e0
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-11 21:16:22 -0600

    Add heading with quote

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,15 @@ function Banner() {
   return (
     <>
       <h1 aria-label="slogan">
-        Look into <strong>the future</strong>, experience it
+        See into <strong>the future</strong>, experience it
         <strong> now</strong>
       </h1>
+      <h2 aria-label="quote">
+        Look into the future, because that is where you are going to spend the
+        rest of your life
+      </h2>
       <button>
-        <Link>Start Shopping</Link>
+        <Link to="./shop">Start Shopping</Link>
       </button>
     </>
   );
