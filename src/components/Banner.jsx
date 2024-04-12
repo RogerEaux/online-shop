552adc6ec3f1a3fe1712448c2b95b37a3ca1b858
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-11 20:20:46 -0600

    Add Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+function Banner() {
+  return (
+    <>
+      <h1 aria-label="slogan">
+        Look into <strong>the future</strong>, experience it
+        <strong> now</strong>
+      </h1>
+    </>
+  );
+}
+
+export default Banner;
