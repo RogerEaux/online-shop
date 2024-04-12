2ce88f568eab326e109a60ef11908e146649f8d3
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-11 21:16:02 -0600

    Test Banner renders heading with quote

--- src/tests/Banner.test.jsx
+++ src/tests/Banner.test.jsx
@@ -11,7 +11,18 @@ describe('Banner component', () => {
     );
     expect(
       screen.getByRole('heading', { name: /slogan/i }).textContent,
-    ).toMatch(/look into the future, experience it now/i);
+    ).toMatch(/see into the future, experience it now/i);
+  });
+
+  it('renders heading with quote', () => {
+    render(
+      <BrowserRouter>
+        <Banner />
+      </BrowserRouter>,
+    );
+    expect(screen.getByRole('heading', { name: /quote/i }).textContent).toMatch(
+      /look into the future, because that is where you are going to spend the rest of your life/i,
+    );
   });
 
   it('renders start shopping button', () => {
