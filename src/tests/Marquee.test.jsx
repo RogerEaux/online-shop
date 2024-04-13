47aeb612113c445f43ef61501a0ae84ef1897ad4
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-12 20:58:33 -0600

    Test marquee renders three paragraphs

---
+++ src/tests/Marquee.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Marquee from '../components/Marquee';
+
+describe('NavBar component', () => {
+  it("renders three paragraphs containing 'The Future is Now is'", () => {
+    render(<Marquee />);
+
+    expect(screen.getAllByText(/the future is now is/i).length).toBe(3);
+  });
+});
