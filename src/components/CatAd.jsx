a49bdce48a560cb5c39c74511eb01a0aeb9ca7fe
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-14 19:50:53 -0600

    Add CatAd styles

--- src/components/CatAd.jsx
+++ src/components/CatAd.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
+import styles from '../styles/CatAd.module.css';
 
-function CatAd({ title, cover }) {
+function CatAd({ title, cover, reverse }) {
   function getImageURL() {
     return new URL(`../assets/${cover}`, import.meta.url).href;
   }
 
   return (
-    <section role="section">
-      <h2>{title}</h2>
-      <img src={getImageURL(cover)} alt={title} />
+    <section role="section" className={reverse ? styles.reverse : styles.catAd}>
+      <h2 className={styles.title}>{title}</h2>
+      <img src={getImageURL(cover)} alt={title} className={styles.cover} />
     </section>
   );
 }
@@ -18,4 +19,5 @@ export default CatAd;
 CatAd.propTypes = {
   title: PropTypes.string,
   cover: PropTypes.string,
+  reverse: PropTypes.bool,
 };
