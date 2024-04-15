4cf5f97694a2bdaa93957fd74f1def7c5e01d3ab
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-14 20:23:39 -0600

    Add heading with promotional message

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,9 @@ import styles from '../styles/Main.module.css';
 function Main() {
   return (
     <main className={styles.main}>
+      <h1 aria-label="Promotional message">
+        Take a look at some of the deals we have to offer!
+      </h1>
       <CatAd title="Best Gaming PCs" cover={'gaming-pc.jpg'} />
       <CatAd title="Best Gaming PCs" cover={'gaming-pc.jpg'} reverse={true} />
     </main>
