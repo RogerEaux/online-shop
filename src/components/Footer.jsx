8f763d473cab6786ec0ef3ba7397a44bed60c062
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-16 14:15:41 -0600

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo-placeholder.png';
+
+function Footer() {
+  return (
+    <footer>
+      <img src={logo} alt="The Company" />
+    </footer>
+  );
+}
+
+export default Footer;
