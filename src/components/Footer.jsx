b615e798177b45ddced9fca6fec280813de5dd3f
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-16 17:21:11 -0600

    Add Footer about and details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,14 @@ function Footer() {
   return (
     <footer>
       <img src={logo} alt="The Company" />
+      <div>
+        <h1 aria-label="about">About The Company</h1>
+        <p>
+          The Company is an online tech commerce seller. Our goal is to inspire
+          people to use and take advantage of recent technologies. The Company
+          offers some of the top and cutting-edge technology.
+        </p>
+      </div>
     </footer>
   );
 }
