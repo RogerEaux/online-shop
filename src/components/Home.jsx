34a317c798af0cc8b092b51862f68afe93647977
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-16 23:55:28 -0600

    Add main page components to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
 import NavBar from './NavBar';
+import Banner from './Banner';
+import Marquee from './Marquee';
+import Main from './Main';
+import Footer from './Footer';
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Banner />
+      <Marquee />
+      <Main />
+      <Footer />
     </>
   );
 }
