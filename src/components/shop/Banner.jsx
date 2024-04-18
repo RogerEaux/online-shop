d2038654fe430e9a26b694db4831a233ec90576f
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-17 20:55:01 -0600

    Add shop Banner component

---
+++ src/components/shop/Banner.jsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/shop/Banner.module.css';
+
+function Banner() {
+  return (
+    <header className={styles.banner}>
+      <h1>Tech Store</h1>
+    </header>
+  );
+}
+
+export default Banner;
