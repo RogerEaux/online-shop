efdd8ba4127cb95af43bfc2e4a52c9644f0c9495
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-20 00:49:58 -0600

    Test ProductCard renders product image

---
+++ src/tests/shop/ProductCard.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import ProductCard from '../../components/shop/ProductCard';
+import { describe, expect, it } from 'vitest';
+
+describe('ProductCard component', () => {
+  it('renders product image', () => {
+    const product = {
+      title: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
+      price: 64,
+      image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+      rating: {
+        rate: 3.3,
+        count: 203,
+      },
+    };
+
+    render(<ProductCard product={product} />);
+
+    expect(
+      screen.getByRole('img', { name: /product image/i }),
+    ).toBeInTheDocument();
+  });
+});
