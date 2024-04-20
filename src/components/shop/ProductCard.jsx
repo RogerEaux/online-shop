baaccc68615793d81c4bde8d217ee4f4da83f397
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-20 00:53:29 -0600

    Add product title to ProductCard

--- src/components/shop/ProductCard.jsx
+++ src/components/shop/ProductCard.jsx
@@ -4,6 +4,7 @@ function ProductCard({ product }) {
   return (
     <section>
       <img src={product.image} alt="product image" />
+      <h2 aria-label="title">{product.title}</h2>
     </section>
   );
 }
