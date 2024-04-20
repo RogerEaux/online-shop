1cb77335c1cee8bda4eafcd9e2a410811eb46f22
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-20 02:00:24 -0600

    Add rate stars to Rating

--- src/components/general/Rating.jsx
+++ src/components/general/Rating.jsx
@@ -1,8 +1,38 @@
+import fullStar from '../../assets/svg/full-star.svg';
+import halfStar from '../../assets/svg/half-star.svg';
+import emptyStar from '../../assets/svg/empty-star.svg';
 import PropTypes from 'prop-types';
 
 function Rating({ rating }) {
+  let full = Math.floor(rating.rate);
+  let half = rating.rate - Math.floor(rating.rate) >= 0.5 ? true : false;
+  const stars = [];
+
+  for (let i = 0; i < 5; i += 1) {
+    if (full) {
+      stars.push('full');
+      full -= 1;
+    } else if (half) {
+      stars.push('half');
+      half = false;
+    } else {
+      stars.push('empty');
+    }
+  }
+
   return (
     <div>
+      <div>
+        {stars.map((star, index) => {
+          return star === 'full' ? (
+            <img key={index} src={fullStar} alt="" />
+          ) : star === 'half' ? (
+            <img key={index} src={halfStar} alt="" />
+          ) : (
+            <img key={index} src={emptyStar} alt="" />
+          );
+        })}
+      </div>
       <p>
         {rating.rate} ({rating.count})
       </p>
