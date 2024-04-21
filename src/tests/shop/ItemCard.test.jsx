17c0d53a6ae6ffbed674768827073a737328239a
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 01:13:54 -0600

    Mock item outside of tests

--- src/tests/shop/ItemCard.test.jsx
+++ src/tests/shop/ItemCard.test.jsx
@@ -3,18 +3,18 @@ import ItemCard from '../../components/shop/ItemCard';
 import { describe, expect, it } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 
+const item = {
+  title: 'foo',
+  price: 42,
+  image: 'bar.jpg',
+  rating: {
+    rate: 4.2,
+    count: 69,
+  },
+};
+
 describe('ItemCard component', () => {
   it('renders item image', () => {
-    const item = {
-      title: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
-      price: 64,
-      image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
-      rating: {
-        rate: 3.3,
-        count: 203,
-      },
-    };
-
     render(
       <BrowserRouter>
         <ItemCard item={item} />
@@ -27,16 +27,6 @@ describe('ItemCard component', () => {
   });
 
   it('renders item title', () => {
-    const item = {
-      title: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
-      price: 64,
-      image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
-      rating: {
-        rate: 3.3,
-        count: 203,
-      },
-    };
-
     render(
       <BrowserRouter>
         <ItemCard item={item} />
@@ -49,16 +39,6 @@ describe('ItemCard component', () => {
   });
 
   it('renders item price', () => {
-    const item = {
-      title: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
-      price: 64,
-      image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
-      rating: {
-        rate: 3.3,
-        count: 203,
-      },
-    };
-
     render(
       <BrowserRouter>
         <ItemCard item={item} />
