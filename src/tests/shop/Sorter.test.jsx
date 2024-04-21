ea8889cdd4c06db5721a271e2235943f3fef80ea
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 01:44:29 -0600

    Test Sorter calls updater function when option is selected

--- src/tests/shop/Sorter.test.jsx
+++ src/tests/shop/Sorter.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Sorter from '../../components/shop/Sorter';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('Sorter component', () => {
   it('renders drop down list with sorters', () => {
-    render(<Sorter />);
+    render(<Sorter updateOption={() => {}} />);
 
     expect(screen.getByLabelText(/sort by/i)).toBeInTheDocument();
     expect(screen.getByRole('combobox', { name: /sort by/i })).toContainElement(
@@ -14,5 +15,16 @@ describe('Sorter component', () => {
       screen.getByRole('option', { name: /a to z/i }),
       screen.getByRole('option', { name: /z to a/i }),
     );
+  });
+
+  it('calls updater function when option is selected', async () => {
+    const updateOption = vi.fn();
+    const user = userEvent.setup();
+
+    render(<Sorter updateOption={updateOption} />);
+
+    await user.selectOptions(screen.getByRole('combobox'), 'A to Z');
+
+    expect(updateOption).toHaveBeenCalled();
   });
 });
