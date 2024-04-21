f5da7612d995d6f9e2653ec2d59afad630ac8640
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 01:21:09 -0600

    Add ItemGrid component

---
+++ src/components/shop/ItemGrid.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import ItemCard from './ItemCard';
+
+function ItemGrid({ items }) {
+  return (
+    <article>
+      {items.map((item) => {
+        return <ItemCard key={item.id} item={item} />;
+      })}
+    </article>
+  );
+}
+
+ItemGrid.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      rating: PropTypes.shape({
+        rate: PropTypes.number,
+        count: PropTypes.number,
+      }),
+    }),
+  ),
+};
+
+export default ItemGrid;
