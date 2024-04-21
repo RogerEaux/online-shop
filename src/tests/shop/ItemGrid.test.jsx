55a6b277bb9e1f7368815b75a1010ccc39e6dd75
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 01:54:33 -0600

    Test ItemGrid renders items sorted low to high price

--- src/tests/shop/ItemGrid.test.jsx
+++ src/tests/shop/ItemGrid.test.jsx
@@ -1,19 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import ItemGrid from '../../components/shop/ItemGrid';
 import { describe, expect, it, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 vi.mock('../../components/shop/ItemCard', () => ({
   default: ({ item }) => (
     <a key={item.id} href="/">
       {item.title}
+      {item.price}
     </a>
   ),
 }));
 
 const items = [
-  { id: 1, title: 'foo' },
-  { id: 2, title: 'bar' },
-  { id: 3, title: 'baz' },
+  { id: 1, title: 'foo', price: 1 },
+  { id: 2, title: 'bar', price: 3 },
+  { id: 3, title: 'baz', price: 2 },
 ];
 
 describe('ItemGrid component', () => {
@@ -29,5 +31,17 @@ describe('ItemGrid component', () => {
     expect(screen.getAllByRole('link')[0].textContent).toMatch('foo');
     expect(screen.getAllByRole('link')[1].textContent).toMatch('bar');
     expect(screen.getAllByRole('link')[2].textContent).toMatch('baz');
+  });
+
+  it('renders items sorted by low to high price', async () => {
+    const user = userEvent.setup();
+
+    render(<ItemGrid items={items} />);
+
+    await user.selectOptions(screen.getByRole('combobox'), 'Price Low to High');
+
+    expect(screen.getAllByRole('link')[0].textContent).toMatch('1');
+    expect(screen.getAllByRole('link')[1].textContent).toMatch('2');
+    expect(screen.getAllByRole('link')[2].textContent).toMatch('3');
   });
 });
