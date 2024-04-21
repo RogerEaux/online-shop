64ca7901a46d18fb136f4f038ed6c5fd1e7f66be
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 02:08:25 -0600

    Fix option value typo

--- src/components/shop/Sorter.jsx
+++ src/components/shop/Sorter.jsx
@@ -8,7 +8,7 @@ function Sorter({ updateOption }) {
       <select name="sort" id="sort" onChange={updateOption}>
         <option value="">Popularity</option>
         <option value="priceltoh">Price Low to High</option>
-        <option value="pricelhtol">Price High to Low</option>
+        <option value="pricehtol">Price High to Low</option>
         <option value="atoz">A to Z</option>
         <option value="ztoa">Z to A</option>
       </select>
