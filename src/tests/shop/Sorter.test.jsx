5575c5cae2a64ff7d76793fc6a62bbe6c3eca89d
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 02:44:36 -0600

    Fix popular option typo

--- src/tests/shop/Sorter.test.jsx
+++ src/tests/shop/Sorter.test.jsx
@@ -9,7 +9,7 @@ describe('Sorter component', () => {
 
     expect(screen.getByLabelText(/sort by/i)).toBeInTheDocument();
     expect(screen.getByRole('combobox', { name: /sort by/i })).toContainElement(
-      screen.getByRole('option', { name: /popularity/i }),
+      screen.getByRole('option', { name: /popular/i }),
       screen.getByRole('option', { name: /price low to high/i }),
       screen.getByRole('option', { name: /price high to low/i }),
       screen.getByRole('option', { name: /a to z/i }),
