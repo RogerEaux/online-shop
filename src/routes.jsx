e10a33ae70ab99659d96f407331e807108effe47
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 23:43:09 -0600

    Add shop route and dynamic shop routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,18 @@
 import Home from './components/home/Home';
+import Shop from './components/shop/Shop';
 
 const routes = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
+  },
+  {
+    path: '/shop/:category',
+    element: <Shop />,
   },
 ];
 
