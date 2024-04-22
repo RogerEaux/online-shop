91c9119e77f0d5ce9d33e81d09f35d3b159671df
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 22:37:43 -0600

    Add RequestError styles and image

--- src/components/shop/RequestError.jsx
+++ src/components/shop/RequestError.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import styles from '../../styles/shop/RequestError.module.css';
 
 function RequestError({ error, retry }) {
   return (
-    <div>
+    <div className={styles.error}>
       <h2 aria-label="error">
         Oops! Looks like you&apos;ve encountered a {error.message}
       </h2>
