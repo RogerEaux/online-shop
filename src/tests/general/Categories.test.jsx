746677cba85e3eb1e136e57dd0d9de55f9f02c79
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 19:27:51 -0600

    Test Categories renders list of category links

---
+++ src/tests/general/Categories.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import Categories from '../../components/general/Categories';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+
+describe('Categories component', () => {
+  it('renders a list of links including all categories', () => {
+    render(
+      <BrowserRouter>
+        <Categories />
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByRole('list')).toContainElement(
+      screen.getByRole('link', { name: /electronics/i }),
+      screen.getByRole('link', { name: /jewelery/i }),
+      screen.getByRole('link', { name: /^men's clothing/i }),
+      screen.getByRole('link', { name: /women's clothing/i }),
+    );
+  });
+});
