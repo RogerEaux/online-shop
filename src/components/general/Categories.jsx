86dfcc036f247c03a98592c7c97dc0a1f1b71a86
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 21:48:05 -0600

    Add Categories styles

--- src/components/general/Categories.jsx
+++ src/components/general/Categories.jsx
@@ -1,21 +1,22 @@
 import { Link } from 'react-router-dom';
+import styles from '../../styles/general/Categories.module.css';
 
 function Categories() {
   return (
-    <ul>
-      <li>
+    <ul className={styles.categories}>
+      <li className={styles.category}>
         <Link to={'/shop'}>All Categories</Link>
       </li>
-      <li>
+      <li className={styles.category}>
         <Link to={'/shop/electronics'}>Electronics</Link>
       </li>
-      <li>
+      <li className={styles.category}>
         <Link to={'/shop/jewelery'}>Jewelery</Link>
       </li>
-      <li>
+      <li className={styles.category}>
         <Link to={'/shop/mens-clothing'}>Men&apos;s Clothing</Link>
       </li>
-      <li>
+      <li className={styles.category}>
         <Link to={'/shop/womens-clothing'}>Women&apos;s Clothing</Link>
       </li>
     </ul>
