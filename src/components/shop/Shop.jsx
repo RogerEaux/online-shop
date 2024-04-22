ce056f0fb1428177c144fd157f85ee4763eb8a15
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 22:42:24 -0600

    Add Shop component

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,17 @@
+import NavBar from '../general/NavBar';
+import Banner from './Banner';
+import Main from './Main';
+import Footer from '../general/Footer';
+
+function Shop() {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Shop;
