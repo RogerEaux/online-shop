ee3b01be40dfbf534d52eede70af8ab824a637ef
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-21 22:18:08 -0600

    Test retry button calls retry function

--- src/tests/shop/RequestError.test.jsx
+++ src/tests/shop/RequestError.test.jsx
@@ -1,19 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import RequestError from '../../components/shop/RequestError';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('RequestError component', () => {
   it('renders error message', () => {
     const error = { message: 'Foo Error' };
 
-    render(<RequestError error={error} />);
+    render(<RequestError error={error} retry={() => {}} />);
 
     expect(screen.getByRole('heading', { name: /error/i }).textContent).toMatch(
       `Oops! Looks like you've encountered a ${error.message}`,
     );
   });
 
-  it.todo('renders retry button');
+  it('calls retry function when clicking retry button', async () => {
+    const retry = vi.fn();
+    const user = userEvent.setup();
 
-  it.todo('calls retry function when clicking retry button');
+    render(<RequestError error={{}} retry={retry} />);
+
+    expect(screen.getByRole('button', { name: /retry/i })).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: /retry/i }));
+
+    expect(retry).toHaveBeenCalled();
+  });
 });
