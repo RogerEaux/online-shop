f5f2fd03be0e3817deced72894632d770b732ba5
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-23 21:13:42 -0600

    Add loading and error screen to ProductCard

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,14 +1,31 @@
 import useFetchProduct from '../../utils/useFetchProduct.jsx';
+import Rating from '../general/Rating.jsx';
+import loadingGIF from '../../assets/images/loading.gif';
+import RequestError from '../general/RequestError';
 
 function ProductCard() {
+  function retry() {
+    location.reload();
+  }
+
   const { product, loading, error } = useFetchProduct();
 
   return (
     <main>
-      <img src={product.image} alt="product image" />
-      <h1 aria-label="title">{product.title}</h1>
-      <p>${product.price}</p>
-      <h2 aria-label="description">{product.description}</h2>
+      {loading ? (
+        <div>
+          <img src={loadingGIF} alt="loading" />
+        </div>
+      ) : error ? (
+        <RequestError error={error} retry={retry} />
+      ) : (
+        <>
+          <img src={product.image} alt="product image" />
+          <h1 aria-label="title">{product.title}</h1>
+          <p>${product.price}</p>
+          <h2 aria-label="description">{product.description}</h2>
+        </>
+      )}
     </main>
   );
 }
