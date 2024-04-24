f4122fde8e2541c18444f47b4b438ed0655d5a74
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-23 22:03:46 -0600

    Remove callback test use routing instead

--- src/tests/general/RequestError.test.jsx
+++ src/tests/general/RequestError.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import RequestError from '../../components/general/RequestError';
-import { describe, expect, it, vi } from 'vitest';
-import userEvent from '@testing-library/user-event';
+import { describe, expect, it } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('RequestError component', () => {
@@ -17,24 +16,5 @@ describe('RequestError component', () => {
     expect(screen.getByRole('heading', { name: /error/i }).textContent).toMatch(
       `Oops! Looks like ${error}`,
     );
-  });
-
-  it('calls action function when clicking action button', async () => {
-    const action = vi.fn();
-    const user = userEvent.setup();
-
-    render(
-      <BrowserRouter>
-        <RequestError action={action} actionButton={'bar'} />
-      </BrowserRouter>,
-    );
-
-    expect(screen.getByRole('link', { name: /action/i }).textContent).toMatch(
-      'bar',
-    );
-
-    await user.click(screen.getByRole('link', { name: /action/i }));
-
-    expect(action).toHaveBeenCalled();
   });
 });
