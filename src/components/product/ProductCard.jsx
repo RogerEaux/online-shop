1de62857e5dbd1625bc303eeaeaccbfc10ac7802
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-23 22:38:01 -0600

    Add ProductCard styles

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -2,12 +2,13 @@ import useFetchProduct from '../../utils/useFetchProduct.jsx';
 import Rating from '../general/Rating.jsx';
 import RequestError from '../general/RequestError';
 import Loading from '../general/Loading.jsx';
+import styles from '../../styles/product/ProductCard.module.css';
 
 function ProductCard() {
   const { product, loading, error } = useFetchProduct();
 
   return (
-    <main>
+    <main className={styles.productCard}>
       {loading ? (
         <Loading />
       ) : error ? (
@@ -18,11 +19,15 @@ function ProductCard() {
         />
       ) : product ? (
         <>
-          <img src={product.image} alt="product image" />
-          <h1 aria-label="title">{product.title}</h1>
-          <p>${product.price}</p>
-          <h2 aria-label="description">{product.description}</h2>
-          <Rating rating={product.rating} />
+          <div className={styles.image}>
+            <img src={product.image} alt="product image" />
+          </div>
+          <div className={styles.details}>
+            <h1 aria-label="title">{product.title}</h1>
+            <Rating rating={product.rating} />
+            <p>${product.price}</p>
+            <h2 aria-label="description">{product.description}</h2>
+          </div>
         </>
       ) : null}
     </main>
