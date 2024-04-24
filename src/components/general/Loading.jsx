1390392b2e30cbb09820435ce5bc99c2027d74e4
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-23 21:59:26 -0600

    Add styles to Loading

--- src/components/general/Loading.jsx
+++ src/components/general/Loading.jsx
@@ -1,8 +1,9 @@
 import loadingGIF from '../../assets/images/loading.gif';
+import styles from '../../styles/general/Loading.module.css';
 
 function Loading() {
   return (
-    <div>
+    <div className={styles.loading}>
       <img src={loadingGIF} alt="loading" />
     </div>
   );
