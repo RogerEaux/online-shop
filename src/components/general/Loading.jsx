3e1eddd343005965417c306dc59e396f1bc03deb
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-23 21:57:52 -0600

    Add Loading component

---
+++ src/components/general/Loading.jsx
@@ -0,0 +1,11 @@
+import loadingGIF from '../../assets/images/loading.gif';
+
+function Loading() {
+  return (
+    <div>
+      <img src={loadingGIF} alt="loading" />
+    </div>
+  );
+}
+
+export default Loading;
