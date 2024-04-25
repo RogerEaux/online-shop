a2ba031ce24494cb94debd363c3f3c7fe1d4a94b
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-25 01:04:39 -0600

    Add router to SearchBar tests

--- src/tests/general/SearchBar.test.jsx
+++ src/tests/general/SearchBar.test.jsx
@@ -1,17 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import SearchBar from '../../components/general/SearchBar';
 import { describe, expect, it } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('SearchBar component', () => {
   it('renders text input', () => {
-    render(<SearchBar />);
+    render(
+      <BrowserRouter>
+        <SearchBar />
+      </BrowserRouter>,
+    );
     expect(
       screen.getByRole('textbox', { name: /search/i }),
     ).toBeInTheDocument();
   });
 
   it('renders search button', () => {
-    render(<SearchBar />);
+    render(
+      <BrowserRouter>
+        <SearchBar />
+      </BrowserRouter>,
+    );
     expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
     expect(screen.getByRole('img', { name: /search/i })).toBeInTheDocument();
   });
