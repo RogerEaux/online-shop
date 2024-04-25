f3f8072ea125dae1e4ce9b609df9658f79194aba
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-25 00:34:58 -0600

    Add method to set search params to search input value

--- src/components/general/SearchBar.jsx
+++ src/components/general/SearchBar.jsx
@@ -1,17 +1,34 @@
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import searchIcon from '../../assets/svg/search.svg';
 import styles from '../../styles/general/SearchBar.module.css';
 
 function SearchBar() {
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
+
+  function search() {
+    navigate(`/shop?search=${inputRef.current.value}`);
+  }
+
+  function searchEnter(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+  }
+
   return (
     <>
       <label className={styles.search} aria-label="search">
         <input
+          onKeyDown={searchEnter}
+          ref={inputRef}
           className={styles.searchBox}
           type="text"
           id="searchBox"
           placeholder="Search for item..."
         />
-        <button className={styles.searchButton}>
+        <button onClick={search} className={styles.searchButton}>
           <img src={searchIcon} alt="Search" />
         </button>
       </label>
