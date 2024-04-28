0005325c2730cc945f2711842c30010c7c4ae645
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 20:02:53 -0600

    Test Cart renders closing button

--- src/tests/general/Cart.test.jsx
+++ src/tests/general/Cart.test.jsx
@@ -1,13 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import Cart from '../../components/general/Cart';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('Cart component', () => {
   it('renders shopping cart title', () => {
-    render(<Cart />);
+    render(<Cart closeCart={() => {}} />);
 
     expect(
       screen.getByRole('heading', { name: /shopping cart/i }).textContent,
     ).toMatch('Shopping Cart');
+  });
+
+  it('renders closing shopping cart button that calls closing function when clicked', async () => {
+    const closeCart = vi.fn();
+    const user = userEvent.setup();
+
+    render(<Cart closeCart={closeCart} />);
+
+    await user.click(screen.getByRole('button', { name: /close/i }));
+
+    expect(closeCart).toHaveBeenCalled();
   });
 });
