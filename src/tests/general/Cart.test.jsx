c5b56d393d1e374e89dd4555ba46d339fab4b3ab
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 22:25:04 -0600

    Test Cart renders items, subtotal and checkout

--- src/tests/general/Cart.test.jsx
+++ src/tests/general/Cart.test.jsx
@@ -2,11 +2,17 @@ import { render, screen } from '@testing-library/react';
 import Cart from '../../components/general/Cart';
 import { describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import { createContext } from 'react';
+import { CartContext } from '../../App';
+
+vi.mock('../../components/general/CartItem', () => ({
+  default: ({ item }) => {
+    return <h2 aria-label="item">{item.title}</h2>;
+  },
+}));
 
 describe('Cart component', () => {
   it('renders shopping cart title', () => {
-    render(<Cart closeCart={() => {}} />);
+    render(<Cart />);
 
     expect(
       screen.getByRole('heading', { name: /shopping cart/i }).textContent,
@@ -25,16 +31,39 @@ describe('Cart component', () => {
   });
 
   it('renders no item message when there are no items in the cart', () => {
-    const CartContext = createContext({});
-
     render(
       <CartContext.Provider value={[]}>
-        <Cart closeCart={() => {}} />
+        <Cart />
       </CartContext.Provider>,
     );
 
     expect(
       screen.getByText('Go add some items to your cart!'),
+    ).toBeInTheDocument();
+  });
+
+  it('renders items, subtotal and checkout button when there are items in cart', () => {
+    render(
+      <CartContext.Provider
+        value={[
+          { id: 1, title: 'foo', price: 42, image: 'foo.jpg' },
+          { id: 2, title: 'bar', price: 69, image: 'bar.jpg' },
+        ]}
+      >
+        <Cart />
+      </CartContext.Provider>,
+    );
+
+    expect(
+      screen.getAllByRole('heading', { name: /item/i })[0].textContent,
+    ).toMatch('foo');
+    expect(
+      screen.getAllByRole('heading', { name: /item/i })[1].textContent,
+    ).toMatch('bar');
+
+    expect(screen.getByText(`Subtotal: $111`)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /checkout/i }),
     ).toBeInTheDocument();
   });
 });
