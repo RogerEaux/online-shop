32148876b8047b82f0e63e4af62df8943669b1d7
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 20:25:14 -0600

    Cart renders no item message when no items in cart

--- src/components/general/Cart.jsx
+++ src/components/general/Cart.jsx
@@ -1,12 +1,17 @@
 import ProtpTypes from 'prop-types';
+import { useContext } from 'react';
+import { CartContext } from '../../App';
 
 function Cart({ closeCart }) {
+  const cartItems = useContext(CartContext);
+
   return (
     <aside>
       <h1>Shopping Cart</h1>
       <button onClick={closeCart} aria-label="Close">
         X
       </button>
+      {cartItems.length ? null : <p>Go add some items to your cart!</p>}
     </aside>
   );
 }
