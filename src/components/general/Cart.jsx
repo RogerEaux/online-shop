efda47b0e1d841c399bae781d8bd855dc8de1a02
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-28 01:40:10 -0600

    Add Cart styles

--- src/components/general/Cart.jsx
+++ src/components/general/Cart.jsx
@@ -2,39 +2,42 @@ import ProtpTypes from 'prop-types';
 import { useContext } from 'react';
 import { CartContext } from '../../App';
 import CartItem from './CartItem';
+import styles from '../../styles/general/Cart.module.css';
 
 function Cart() {
   const { cartItems, deleteItem, plusItem, minusItem } =
     useContext(CartContext);
 
   return (
-    <aside>
-      <h1>Shopping Cart</h1>
-      <button aria-label="Close">X</button>
-      {cartItems.length ? (
-        <>
-          <section>
-            {cartItems.map((item) => {
-              return (
-                <CartItem
-                  key={item.id}
-                  item={item}
-                  deleteItem={deleteItem}
-                  plusItem={plusItem}
-                  minusItem={minusItem}
-                />
-              );
-            })}
-          </section>
-          <p>
-            Subtotal: ${cartItems.reduce((acc, curr) => acc + curr.price, 0)}
-          </p>
-          <button>Checkout</button>
-        </>
-      ) : (
-        <p>Go add some items to your cart!</p>
-      )}
-    </aside>
+    <div className={styles.modal}>
+      <aside className={styles.cart}>
+        <h1>Shopping Cart</h1>
+        <button aria-label="Close">X</button>
+        {cartItems.length ? (
+          <div className={styles.items}>
+            <section>
+              {cartItems.map((item) => {
+                return (
+                  <CartItem
+                    key={item.id}
+                    item={item}
+                    deleteItem={deleteItem}
+                    plusItem={plusItem}
+                    minusItem={minusItem}
+                  />
+                );
+              })}
+            </section>
+            <p>
+              Subtotal: ${cartItems.reduce((acc, curr) => acc + curr.price, 0)}
+            </p>
+            <button>Checkout</button>
+          </div>
+        ) : (
+          <p className={styles.noItems}>Go add some items to your cart!</p>
+        )}
+      </aside>
+    </div>
   );
 }
 
