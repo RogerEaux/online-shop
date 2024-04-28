8bc4ef73cb55c486629bad30163d5ad1645ddab9
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 20:44:07 -0600

    CartItem renders item title

--- src/components/general/CartItem.jsx
+++ src/components/general/CartItem.jsx
@@ -1,9 +1,24 @@
-function CartItem() {
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function CartItem({ item }) {
   return (
     <div>
-      <img src="" alt="item image" />
+      <Link to={item.id}>
+        <img src={item.image} alt="item image" />
+      </Link>
+      <h2>{item.title}</h2>
     </div>
   );
 }
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    image: PropTypes.string,
+  }),
+};
 
 export default CartItem;
