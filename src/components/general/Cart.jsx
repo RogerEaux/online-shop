9f2d8b3a811f4581864a2f926d8e681a80c7c0f8
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 20:03:15 -0600

    Add button to close Cart when clicked

--- src/components/general/Cart.jsx
+++ src/components/general/Cart.jsx
@@ -1,9 +1,18 @@
-function Cart() {
+import ProtpTypes from 'prop-types';
+
+function Cart({ closeCart }) {
   return (
     <aside>
       <h1>Shopping Cart</h1>
+      <button onClick={closeCart} aria-label="Close">
+        X
+      </button>
     </aside>
   );
 }
 
 export default Cart;
+
+Cart.propTypes = {
+  closeCart: ProtpTypes.func,
+};
