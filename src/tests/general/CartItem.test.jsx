fed169d7bb831c7fb2370424c17c66f7da51a462
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-27 20:43:48 -0600

    Test CartItem render item title

--- src/tests/general/CartItem.test.jsx
+++ src/tests/general/CartItem.test.jsx
@@ -1,13 +1,32 @@
 import { render, screen } from '@testing-library/react';
-import CartItem from '../components/CartItem';
+import CartItem from '../../components/general/CartItem';
 import { describe, expect, it } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+
+const item = { title: 'foo', price: 42 };
 
 describe('CartItem component', () => {
   it('renders item image', () => {
-    render(<CartItem />);
+    render(
+      <BrowserRouter>
+        <CartItem item={item} />
+      </BrowserRouter>,
+    );
 
     expect(
       screen.getByRole('img', { name: /item image/i }),
     ).toBeInTheDocument();
+  });
+
+  it('renders item title', () => {
+    render(
+      <BrowserRouter>
+        <CartItem item={item} />
+      </BrowserRouter>,
+    );
+
+    expect(
+      screen.getByRole('heading', { name: item.title }).textContent,
+    ).toMatch(item.title);
   });
 });
