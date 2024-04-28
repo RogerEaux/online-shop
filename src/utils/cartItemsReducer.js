26ee5984f40b18cf5fd7f11a75b4e7ee24f82949
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-28 15:47:40 -0600

    Fix reducer adding copies of the same item to cart

--- src/utils/cartItemsReducer.js
+++ src/utils/cartItemsReducer.js
@@ -1,6 +1,16 @@
 function cartItemsReducer(cartItems, action) {
   switch (action.type) {
     case 'add_item': {
+      const targetItem = cartItems.find((item) => item.id === action.item.id);
+      if (targetItem) {
+        const newCartItems = cartItems.filter(
+          (item) => item.id !== action.item.id,
+        );
+        const newItem = { ...targetItem, quantity: targetItem.quantity + 1 };
+
+        return [...newCartItems, newItem];
+      }
+
       return [...cartItems, action.item];
     }
 
