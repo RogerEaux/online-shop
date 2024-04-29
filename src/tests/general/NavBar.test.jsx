60b3766c33de465ddaa4ae3d4335488b4939d588
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-29 15:52:08 -0600

    Test NavBar renders menu that opens when clicked

--- src/tests/general/NavBar.test.jsx
+++ src/tests/general/NavBar.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import NavBar from '../../components/general/NavBar';
 import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 describe('NavBar component', () => {
   it("renders logo of 'The Company'", () => {
@@ -10,6 +11,7 @@ describe('NavBar component', () => {
         <NavBar />
       </BrowserRouter>,
     );
+
     expect(screen.getByRole('link', { name: /the company/i })).toContainElement(
       screen.getByRole('img', { name: /the company/i }),
     );
@@ -21,8 +23,25 @@ describe('NavBar component', () => {
         <NavBar />
       </BrowserRouter>,
     );
+
     expect(
       screen.getByRole('button', { name: /shopping cart/i }),
     ).toBeInTheDocument();
+  });
+
+  it('renders burger menu icon that when clicked opens menu', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByRole('button', { name: /menu/i })).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: /menu/i }));
+
+    expect(screen.getByRole('menu')).toHaveClass(/visible/i);
   });
 });
