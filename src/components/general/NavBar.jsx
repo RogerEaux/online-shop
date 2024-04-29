b0fa17e6d13413ec0e70c1c992af67bf68bfa305
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-04-29 15:52:53 -0600

    Add menu including input and Links to NavBar

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -2,24 +2,48 @@ import Links from './Links';
 import SearchBar from './SearchBar';
 import logo from '../../assets/svg/logo.svg';
 import cart from '../../assets/svg/cart.svg';
+import burger from '../../assets/svg/burger.svg';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/general/NavBar.module.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../../App';
 import useScroll from '../../utils/useScroll';
 
 function NavBar() {
   const { toggleCart } = useContext(CartContext);
   const { isHidden } = useScroll();
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  function toggleMenu() {
+    if (isMenuActive) {
+      setIsMenuActive(false);
+    } else {
+      setIsMenuActive(true);
+    }
+  }
 
   return (
     <nav className={`${styles.navBar} ${isHidden ? styles.hidden : ''}`}>
-      <Link to="/" className={styles.logo}>
+      <Link
+        to="/"
+        className={`${styles.logo} ${isMenuActive ? styles.hidden : ''}`}
+      >
         <img src={logo} alt="The Company" />
       </Link>
-      <SearchBar />
-      <Links direction="row" />
-      <button onClick={toggleCart} className={styles.cart}>
+      <button className={styles.burgerButton} onClick={toggleMenu}>
+        <img src={burger} alt="Menu" />
+      </button>
+      <div
+        role="menu"
+        className={`${styles.burgerMenu} ${isMenuActive ? styles.visible : ''}`}
+      >
+        <SearchBar />
+        <Links direction="row" />
+      </div>
+      <button
+        onClick={toggleCart}
+        className={`${styles.cart} ${isMenuActive ? styles.hidden : ''}`}
+      >
         <img src={cart} alt="Shopping Cart" />
       </button>
     </nav>
